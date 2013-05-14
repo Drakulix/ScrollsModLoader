d2436f7ea6a98a4172c4e12a1b34eae6545e6629
Victor Brekenfeld (Drakulix) <drakulix@portingteam.com>
2013-05-14 23:32:19 +0200

    Project Creation

---
+++ Patcher.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ScrollsModLoader
+{
+	class Patcher
+	{
+		public static void Main (string[] args)
+		{
+			/* TO-DO
+			 * get path of Scrolls
+			 * backup old Assembly
+			 * patch it
+			 * run ILRepack and inject self
+			 */
+
+		}
+	}
+}
