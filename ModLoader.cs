2c5a15d8445f548ae27ad1baf0323c31587918a4
Victor Brekenfeld (Drakulix) <drakulix@portingteam.com>
2013-05-24 16:14:53 +0200

    First version of working patcher code

--- ModLoader.cs
+++ ModLoader.cs
@@ -1,12 +1,65 @@
 using System;
+using LinFu.AOP.Interfaces;
 
-namespace ScrollsModLoader
-{
-	public class ModLoader
+//namespace ScrollsModLoader {
+
+	/*
+	 * handels mod loading and debug file logging
+	 * actual patches like logging network traffic, should be seperated in the future
+	 * this class should only pass invokes through itself to the right mods
+	 * 
+	 * TO-DO: Add actual ModInterface ;)
+	 * 
+	 */
+
+	public class CommunicatorReplace : IInterceptor
 	{
-		public ModLoader ()
+
+		public object Intercept (IInvocationInfo info)
 		{
+			ModLoader.Log("Client: "+info.Arguments[0]);
+			return info.TargetMethod.Invoke (info.Target, info.Arguments);
+		}
+
+	}
+
+	public class SimpleMethodReplacementProvider : BaseMethodReplacementProvider 
+	{
+		protected override IInterceptor GetReplacement (object host, IInvocationInfo context)
+		{
+			return new CommunicatorReplace ();
 		}
 	}
-}
 
+	public class ModLoader : ICommListener
+	{
+		static ModLoader instance = null;
+		private System.IO.StreamWriter log = null;
+
+		public ModLoader ()
+		{
+			log = System.IO.File.CreateText("log.txt");
+		}
+
+		public static void Log(string s) {
+			instance.log.WriteLine(s);
+			instance.log.Flush ();
+		}
+
+		#region ICommListener implementation
+		public void handleMessage (Message msg)
+		{
+			ModLoader.Log("Server: "+msg);
+		}
+		#endregion
+		
+		//initial game callback
+		public static void Init() {
+			instance = new ModLoader();
+			App.Communicator.addListener(instance);
+			MethodBodyReplacementProviderRegistry.SetProvider(new SimpleMethodReplacementProvider());
+			
+			//not needed I hope
+			//((IModifiableType)App.Communicator).IsInterceptionDisabled = false;
+		}
+	}
--- Patcher.cs
+++ Patcher.cs
@@ -1,50 +1,115 @@
 using System;
+using System.Collections;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using LinFu.AOP.Cecil.Extensions;
+using LinFu.Reflection.Emit;
 
 namespace ScrollsModLoader
 {
 	class Patcher
 	{
+
+		/*
+		 *  Actual patcher class
+		 *  Makes use of Mono.Cecil and Linfu
+		 * 
+		 *  May be called directly via Main for first time patches
+		 *  or from the inside of modloader to adjust(re-patch) the (backup)assembly for new game/mod/modloader updates
+		 * 
+		 */
+
 		public static void Main (string[] args)
 		{
-			/* TO-DO
-			 * get path of Scrolls
-			 * backup old Assembly
-			 * patch it
-			 * run ILRepack and inject self
-			 */
-
 
 			//get Path of Scrolls Data Folder
-			//Note: AllowedFileTypes is currently broken on MonoMac. it freezes the app
-
 			String installPath = Platform.getGlobalScrollsInstallPath();
 			if (installPath == null) return;
 
 
 			//create modloader folder
-			if (!System.IO.Directory.Exists(installPath+"/Managed/ModLoader/")) {
-				System.IO.Directory.CreateDirectory(installPath+"/Managed/ModLoader/");
+			if (!System.IO.Directory.Exists(installPath+"Managed/ModLoader/")) {
+				System.IO.Directory.CreateDirectory(installPath+"Managed/ModLoader/");
 			}
 
 			//backup original assembly
-			System.IO.File.Copy(installPath+"/Managed/Assembly.CSharp.dll", installPath+"/Managed/ModLoader/Assembly.CSharp.dll");
+			if (!System.IO.File.Exists(installPath+"Managed/ModLoader/Assembly-CSharp.dll"))
+				System.IO.File.Copy (installPath+"Managed/Assembly-CSharp.dll", installPath + "Managed/ModLoader/Assembly-CSharp.dll");
+			else {
+			//if a backup already exists, it is much more likely that the current assembly is messed up and the user wants to repatch
+				System.IO.File.Delete(installPath+"Managed/Assembly-CSharp.dll");
+				System.IO.File.Copy(installPath+"Managed/ModLoader/Assembly-CSharp.dll", installPath+"Managed/Assembly-CSharp.dll");
+			}
 
-
+			Console.WriteLine ("Patching...");
 			//patch it
 			Patcher patcher = new Patcher();
-			if (!patcher.patchAssembly(installPath+"/Managed/ModLoader/Assembly.CSharp.dll")) {
+			if (!patcher.patchAssembly(installPath+"Managed/Assembly-CSharp.dll")) {
 				Console.WriteLine("Patching failed");
+				//don't save patch at this point. If the "real" patcher fails, we should tell the user instead
+				//save-patching is for installs, that get broken by updates, etc, to keep the install until ScrollsModLoader is updated
+				Dialogs.showNotification ("Patching failed", "ScrollsModLoader was unable to prepare your client, you are likely using an incompatible version. More at ScrollsGuide.com");
 			}
+			Console.WriteLine ("Done");
+
+			return;
 		}
 
 
 		public bool patchAssembly(String path) {
+			//"weave" the assembly
+			if (!weaveAssembly (path))
+				return false;
+			Console.WriteLine ("Weaved Assembly");
+
+			/*
+			 * add init hack
+			 */
+
+			try {
+				//load assembly
+				Hooks.loadBaseAssembly(path);
+				//load self
+				Hooks.loadInjectAssembly(System.Reflection.Assembly.GetExecutingAssembly().Location);
+			} catch (Exception exp) {
+				//something must be gone horribly wrong if it crashes here
+				Console.WriteLine (exp);
+				return false;
+			}
+
+			//add hook
+			if (!Hooks.hookStaticVoidMethodAtEnd ("App.Awake", "ModLoader.Init"))
+				return false;
+
+			try {
+				//save assembly
+				Hooks.savePatchedAssembly();
+				
+				//inject self
+				System.Reflection.Assembly.Load (System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("ScrollsModLoader.ILRepack.exe").ReadToEnd()).EntryPoint.Invoke(null, new object[] { new string[] {"/out:"+path, path, System.Reflection.Assembly.GetExecutingAssembly().Location} });
+			} catch (Exception exp) {
+				//also very unlikely, but for safety
+				Console.WriteLine (exp);
+				return false;
+			}
 
 			return true;
 		}
 
-		public bool saveModePatchAssembly(String path) {
+		public bool weaveAssembly(String path) {
+			// let LinFu inject some call hooks into all required classes and methods to replace/extend method calls
+			try {
+				AssemblyDefinition assembly = AssemblyFactory.GetAssembly(path);
+				assembly.InterceptMethodBody (new ScrollsFilter.ScrollsMethodFilter(), new ScrollsFilter.ScrollsTypeFilter());
+				assembly.Save(path);
+				return true;
+			} catch (Exception exp) {
+				Console.WriteLine (exp);
+				return false;
+			}
+		}
 
+		public bool saveModePatchAssembly(String path) {
 			return true;
 		}
 	}
